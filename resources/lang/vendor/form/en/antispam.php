8b48bc7ead34e835dac9965141d1bb8fb93734ee
mahdi <dr.mahdikazemizade84@gmail.com>
2024-04-07 05:25:02 +0330

    skiping for nfc theme add change config liara

---
+++ resources/lang/vendor/form/en/antispam.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'honeypot' => 'Spam detected.',
+    'timetrap' => 'Spam detected. Please wait a few seconds before submitting the form again.',
+];
---
+++ resources/lang/vendor/form/fr/antispam.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'honeypot' => 'Spam detecté',
+    'timetrap' => 'Spam detecté. Veuillez patienter quelques secondes avant de soumettre à nouveau le formulaire.',
+];
